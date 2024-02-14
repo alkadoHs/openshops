4ed260e170a425de67fcae6cfc64bca62d6195be
alkadoHs <alkadosichone@gmail.com>
2024-02-14 15:04:15 +0300

    show sent stock and new stock to b.transfer

--- app/Filament/Resources/BranchTransferResource/Pages/ListBranchTransfers.php
+++ app/Filament/Resources/BranchTransferResource/Pages/ListBranchTransfers.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\BranchTransferResource\Pages;
 
 use App\Filament\Resources\BranchTransferResource;
+use App\Models\BranchTransfer;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListBranchTransfers extends ListRecords
 {
@@ -14,6 +17,22 @@ class ListBranchTransfers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->badge(BranchTransfer::query()->count()),
+            'new_stock' => Tab::make('New stock')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('to_branch_id', auth()->user()->branch_id)->where('status', 'pending'))
+                ->badge(BranchTransfer::query()->where('to_branch_id', auth()->user()->branch_id)->where('status', 'pending')->count())
+                ->badgeColor('success'),
+            'sent_stock' => Tab::make('Sent stock')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('from_branch_id', auth()->user()->branch_id)->where('status', 'pending'))
+                ->badge(BranchTransfer::query()->where('from_branch_id', auth()->user()->branch_id)->where('status', 'pending')->count())
+                ->badgeColor('warning'),
         ];
     }
 }
