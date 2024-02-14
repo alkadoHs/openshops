943cd2d8cab8307eb1825bed87735df5e2877baf
alkadoHs <alkadosichone@gmail.com>
2024-02-14 13:50:31 +0300

    credit auth

--- app/Filament/Resources/CreditOrderResource/Pages/EditCreditOrder.php
+++ app/Filament/Resources/CreditOrderResource/Pages/EditCreditOrder.php
@@ -13,7 +13,7 @@ class EditCreditOrder extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 }
