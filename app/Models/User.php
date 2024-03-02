b6ba9ed8c0ee2004e9c960367aae56ed77eb3836
alkadoHs <alkadosichone@gmail.com>
2024-03-02 18:41:22 +0300

    access panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable implements FilamentUser
      */
     public function canAccessPanel(Panel $panel): bool
     {
-        return true;
+        return str_ends_with($this->email, '@gmail.com');
     }
 
     /**
